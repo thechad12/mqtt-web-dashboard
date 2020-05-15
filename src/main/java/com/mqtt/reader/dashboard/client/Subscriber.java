4c7df62e5b9aafec419eb8d55a9f28ce38eba302
Kevin Pasquarella <kpasquarella@TWOROADS.local>
2020-05-15 11:15:24 -0400

    mqtt subscriber

--- src/main/java/com/mqtt/reader/dashboard/client/Subscriber.java
+++ src/main/java/com/mqtt/reader/dashboard/client/Subscriber.java
@@ -1,4 +1,41 @@
 package com.mqtt.reader.dashboard.client;
 
-public class Subscriber {
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.lang.Throwable;
+import java.util.UUID;
+
+
+public class Subscriber implements MqttCallback {
+
+    private String serverURI;
+    private MqttAsyncClient client;
+    private String clientID = UUID.randomUUID().toString();
+
+    public Subscriber(String serverURI) throws MqttException {
+        this.serverURI = serverURI;
+        this.client = new MqttAsyncClient(serverURI,clientID,new MemoryPersistence());
+        this.client.setCallback(this);
+        this.client.connect();
+    }
+
+    @Override
+    public void connectionLost(Throwable ex) {
+
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken token) {
+
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage message) {
+
+    }
 }
